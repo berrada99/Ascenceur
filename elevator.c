81af6fdf621c09b7248aef9b8aa55c293d7d041d
Hassan <hassan.berrada99@gmail.com>
2020-07-31 21:35:49 +0200

    Setting up elevator.c

---
+++ elevator.c
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <stdlib.h>
+#include <ncurses.h>
+
+#include"elevator.h"
+#include"person.h"
+
+
+Elevator *create_elevator(int capacity, int currentFloor, PersonList *persons);
+
+Building *create_building(int nbFloor, Elevator *elevator, PersonList **waitingLists);
+
+PersonList* exitElevator(Elevator *e);
+
+PersonList* enterElevator(Elevator *e, PersonList *list);
+
+void stepElevator(Building *b);
